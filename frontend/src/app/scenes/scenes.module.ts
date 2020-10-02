c8d9dcba70e70b06eb68fcf3aecb745774165b7b
Vitalii Yarmus <55307415+325Vitalik@users.noreply.github.com>
2020-10-02 20:32:29 +0300

    add prettier and configure pre-commit hooks

--- frontend/src/app/scenes/scenes.module.ts
+++ frontend/src/app/scenes/scenes.module.ts
@@ -2,13 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SomeFetureModule } from './some-feture/some-feture.module';
 
-
-
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    SomeFetureModule
-  ]
+    declarations: [],
+    imports: [CommonModule, SomeFetureModule],
 })
-export class ScenesModule { }
+export class ScenesModule {}

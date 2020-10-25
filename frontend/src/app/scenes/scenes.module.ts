672ac3714384dd287c487cd7f7935e82c5f0de4b
Vitalii Yarmus <55307415+325Vitalik@users.noreply.github.com>
2020-10-25 18:59:12 +0200

    delete some-feature, now we have one modules where all component will be

--- frontend/src/app/scenes/scenes.module.ts
+++ frontend/src/app/scenes/scenes.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SomeFetureModule } from './some-feture/some-feture.module';
 
 @NgModule({
     declarations: [],
-    imports: [CommonModule, SomeFetureModule],
+    imports: [CommonModule],
 })
 export class ScenesModule {}
